905f86e47d8c84a3e637238ac4d24d883a1e738a
StyleTang <396858275@qq.com>
2016-12-08 00:28:24 +0800

    import java.util.*;
    import java.util.*;

--- rocketmq-store/src/main/java/com/alibaba/rocketmq/store/StoreStatsService.java
+++ rocketmq-store/src/main/java/com/alibaba/rocketmq/store/StoreStatsService.java
@@ -22,9 +22,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.text.MessageFormat;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.locks.ReentrantLock;
